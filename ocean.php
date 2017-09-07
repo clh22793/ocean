92c238b52c0487c05124b0de185ec365b6cdca8f
root <chris.hickey@gmail.com>
2017-09-07 04:58:11 +0000

    removing DB_Connection from ocean.php

--- ocean.php
+++ ocean.php
@@ -1,45 +1,5 @@
 <?php 
 
-class IndexException extends Exception {
-	protected $message = "package not indexed";
-	protected $code = 1001;
-}
-
-class DB_Connection {
-	private $resource;
-
-	public function __construct($host, $user, $pw, $db){
-		$this->resource = mysqli_connect($host, $user, $pw, $db);
-		/* check connection */
-		if (!$this->resource) {
-			echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			exit;
-		}
-	}
-
-	public function get_resource(){
-		return $this->resource;
-	}
-
-	public function real_escape_string($string){
-		return $this->resource->real_escape_string($string);
-	}
-
-	public function query($query){
-		$result = $this->resource->query($query);
-/*
-		echo "\n\n";
-		echo $query."\n";
-		echo $this->resource->error;
-*/
-		return $result;
-
-	}
-
-}
-
 class PackageIndexer {
 
 	private $db_connection;
@@ -55,12 +15,8 @@ class PackageIndexer {
 			return "FAIL";
 		}
 
-		try{
-			if($package_records = $this->get_indexed_packages([$package])){
-				$this->clear_dependencies($package_records[0]);
-			}
-		} catch (IndexException $e) {
-			// do nothing
+		if($package_records = $this->get_indexed_packages([$package])){
+			$this->clear_dependencies($package_records[0]);
 		}
 
 		$package_id = $this->add_index($package);
@@ -106,42 +62,19 @@ class PackageIndexer {
 			$active = 1;
 			$result = $this->db_connection->query("select * from packages where name in ({$package_name_list}) and active = {$active}");
 			
-//var_dump($result);
 			while($row = $result->fetch_assoc()){
 				$indexed_packages[] = $row;
 			}
 		}
 
 		return $indexed_packages;
-
-/*
-		foreach($packages as $package){
-			$package = $this->db_connection->real_escape_string(trim($package));
-			$active = 1;
-			$result = $this->db_connection->query("select * from packages where name = '{$package}' and active = {$active}");
-
-			if($result->num_rows == 0){
-				// todo: resolve this issue
-//				throw new IndexException("could not find package ({$package})");
-				throw new IndexException();
-			} else {
-				$indexed_packages[] = $result->fetch_assoc();
-			}
-		}		
-
-		return $indexed_packages;
-*/
-
 	}
 
 	public  function remove_package($name){
 		$name = trim($name);
 
-		// if $name is a dependency, throw error
 		if($this->is_dependency($name)){
-			//throw new Exception("cannot remove an active dependency");
 			return "FAIL";
-		// else, remove $name from index
 		} else if ($record = $this->get_package_by_name($name)){
 			$active = 0;
 			$package_id = $record['id'];
@@ -152,7 +85,6 @@ class PackageIndexer {
 	}
 
 	public  function query_package($name){
-		// if $name is index, return OK
 		$records = $this->get_indexed_packages([trim($name)]);
 
 		if(empty($records)){
@@ -160,8 +92,6 @@ class PackageIndexer {
 		} else {
 			return "OK";
 		}
-		
-		//return $records;
 	}
 
 	public function get_package_by_name($name){
@@ -193,7 +123,6 @@ class PackageIndexer {
 	public function is_dependency($name){
 		$active = 1;
 		$name = $this->db_connection->real_escape_string(trim($name));
-//		$result = $this->db_connection->query("select * from packages p inner join package_dependencies pd on p.id = pd.dependency_id where p.name = '{$name}' and pd.active = {$active}");
 		$result = $this->db_connection->query("select * from packages p1 inner join package_dependencies pd on p.id = pd.dependency_id inner join packages p2 on p2.id = pd.package_id where p1.name = '{$name}' and pd.active = {$active} and p2.active = {$active}");
 
 		if(isset($result->num_rows)){
@@ -205,21 +134,3 @@ class PackageIndexer {
 	
 }
 
-
-$host = "localhost";
-$user = "ocean-tester";
-$pw = "test";
-$db = "oceanPi";
-
-//$PI = new PackageIndexer(new DB_Connection($host, $user, $pw, $db));
-/*$PI->add_package("test1");
-$PI->add_package("test2");
-$PI->add_package("test3", ["test1"]);
-$PI->add_package("test4", ["test1", "test3"]);
-//$PI->add_package("test5", ["test1", "notpresent"]);
-//$PI->remove_package("test2");
-$PI->remove_package("test1");
-//$PI->remove_package("notpresent");
-$PI->query_package("test1");
-$PI->query_package("notpresent");
-*/

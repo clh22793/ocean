3189b12d65d0a651853b9902bc47fc7d1a76182a
root <chris.hickey@gmail.com>
2017-09-07 20:22:35 +0000

    adding config.ini for db creds

--- server.php
+++ server.php
@@ -1,43 +1,49 @@
 <?php
 require_once('index_server.php');
 
-$timeout = 60;
+class IndexManager {
+	const TIMEOUT = 60;
+
+	public function __construct($socket_host, $socket_port){
+		$this->socket = stream_socket_server('tcp://'.$socket_host.':'.$socket_port, $errno, $errstr);
+		if (!$this->socket){
+			echo "$errstr ($errno)<br />\n";
+			exit;
+		}
+
+		$this->config = parse_ini_file("config.ini");
+	}
+
+	public function start($socket_max_threads, $max_idle_time){
+		for($i=0; $i < $socket_max_threads; $i++){
+			$pid = pcntl_fork();
+			if(!$pid){
+				$start_time = time();
+				$idle_time = 0;
+				$server = new IndexServer($this->socket, new PackageIndexer(new DB_Connection($this->config['db_host'], $this->config['db_user'], $this->config['db_pw'], $this->config['db_name'])));
+
+				while($idle_time < $max_idle_time){
+					$conn = stream_socket_accept($this->socket, self::TIMEOUT);
+					$idle_time = time() - $start_time;
+					while($conn && $message = fread($conn, 1024)) {
+						$response = $server->process_message($message);		
+						stream_socket_sendto($conn, $response."\n");
+						$idle_time = 0;
+					}
+				}
+
+				exit;
+			}
+		}
+
+	}
+}
+
 
 $socket_host = "localhost";
 $socket_port = 8080;
 $socket_max_threads = 10;
+$max_idle_time = 60 * 5;
 
-$db_host = "localhost";
-$db_user = "ocean-tester";
-$db_pw = "test";
-$db_name = "oceanPi";
-
-$socket = stream_socket_server('tcp://'.$socket_host.':'.$socket_port, $errno, $errstr);
-if (!$socket){
-	echo "$errstr ($errno)<br />\n";
-	exit;
-}
-
-for($i=0; $i < $socket_max_threads; $i++){
-    $pid = pcntl_fork();
-    if(!$pid){
-		$start_time = time();
-		$max_idle_time = 60 * 5;
-		$idle_time = 0;
-		$server = new IndexServer($socket, new PackageIndexer(new DB_Connection($db_host, $db_user, $db_pw, $db_name)));
-
-		while($idle_time < $max_idle_time){
-			$conn = stream_socket_accept($socket, $timeout);
-			$idle_time = time() - $start_time;
-            while($conn && $message = fread($conn, 1024)) {
-				$response = $server->process_message($message);		
-				stream_socket_sendto($conn, $response."\n");
-				$idle_time = 0;
-			}
-		}
-
-		exit;
-    }
-}
-
-
+$man = new IndexManager($socket_host, $socket_port);
+$man->start($socket_max_threads, $max_idle_time);

341e50222913274e2c7028030df57d07fc442a78
root <chris.hickey@gmail.com>
2017-09-09 13:48:14 +0000

    updating test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("../db_connection.php");
-require_once("../package_indexer.php");
-require_once("../index_server.php");
-require_once("../package_index_manager.php");
+require_once("../source/db_connection.php");
+require_once("../source/package_indexer.php");
+require_once("../source/index_server.php");
+require_once("../source/package_index_manager.php");
